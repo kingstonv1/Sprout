ccf4e320f7575d29f14a6a9ae6078d429a38c5a2
kingstonv1 <kingstongise@gmail.com>
2023-05-15 13:38:37 -0400

    Updated HTML Injector to work in main.js

--- modules/html-injector.js
+++ modules/html-injector.js
@@ -3,23 +3,19 @@ const fs = require('fs');
 // Takes the path to an HTML file and returns the contents of the file with every "injected" tag replaced by the injected HTML.
 function injectHTML(pathToFile) {
     const re = /(?<=<injected component=").*(?=")/g
-    let markup = fs.readFileSync('../site/sprout-central.html', 'utf-8').split('\n');
+    let markup = fs.readFileSync('./site/sprout-central.html', 'utf-8').split('\n');
     
     for (let i = 0; i < markup.length; i++) {
         line = markup[i];
         match = re.exec(line);
         
         if (match != null) {
-            injectedContent = fs.readFileSync(`../site/components/${match}.html`, 'utf-8').split('\n');
-            console.log(markup);
-            console.log(injectedContent);
+            injectedContent = fs.readFileSync(`./site/components/${match}.html`, 'utf-8').split('\n');
             markup.splice(i, 1, ...injectedContent);
-            console.log(markup);
-
         }
     }
 
-    return markup;
+    return markup.join('\n');
 }
 
-console.log(injectHTML());
+module.exports['injectHTML'] = injectHTML;
