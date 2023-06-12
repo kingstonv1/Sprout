4095c7897398c751b9a85a23abc040267547ee5b
kingstonv1 <kingstongise@gmail.com>
2023-06-11 21:36:35 -0400

    Implement HTML Injector

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 let express = require('express');
 let app = express();
 let path = require('path');
+let injectHTML = require('./modules/html-injector.js')['injectHTML'];
 
 app.use(express.static(__dirname + '/site'));
 
 app.get('/', function (req, res) {
+    
     if (req.originalUrl == '/') {
-        res.sendFile(path.resolve('./site/sprout-central.html'))
-    } else {
-        res.sendFile(path.resolve("./site/" + req.originalUrl));
-    }
+        p = path.resolve('./site/sprout-central.html');
+
+        res.send(injectHTML(p));
+        return;
+    } 
+        
+    res.sendFile(path.resolve("./site/" + req.originalUrl));
+    return;
 });
 
 
